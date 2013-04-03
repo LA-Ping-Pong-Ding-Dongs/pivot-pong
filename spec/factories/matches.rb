66ecf5c5dc31f2de414c37210cedc93b4b98f4ff
Jonathan Barnes and Michael McCormick <pair+jonathan+mc@pivotallabs.com>
2013-04-02 21:17:02 -0700

    Adds doubles_match factory (still failing)

--- spec/factories/matches.rb
+++ spec/factories/matches.rb
@@ -3,4 +3,9 @@ FactoryGirl.define do
     association :winner, factory: :player
     association :loser, factory: :player
   end
+
+  factory :doubles_match, class: :match do
+    association :winner, factory: :team
+    association :loser, factory: :team
+  end
 end
