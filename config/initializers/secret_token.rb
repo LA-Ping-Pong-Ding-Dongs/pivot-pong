399ae37d893945c01cfca7f8f1c435ba412962d4
Michael McCormick <pair+mc@pivotallabs.com>
2014-02-18 18:12:48 -0800

    Fresh Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PivotPong::Application.config.secret_key_base = 'a1a83560dcfdaa593ccd744977628db46408b4350b1ca92d40bca388623f6c198a07ce4ed56e3e1e4f395f62d8e70541b15b6e2714df02cdf0a9266aac865726'
