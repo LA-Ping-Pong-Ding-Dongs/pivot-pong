8f70fc2a56b213aee713a77be5b5c5fa997a2d9d
Brian Jennings and Mariana Lenetis <pair+bjennings+mariana@pivotallabs.com>
2014-04-03 17:45:50 -0700

    Fixes bug to reload leaderboard/matches overlay
    [finishes #68850576]

--- app/assets/javascripts/reloadDashboard.js
+++ app/assets/javascripts/reloadDashboard.js
@@ -2,4 +2,6 @@ window.pong = window.pong || {};
 
 pong.reloadDashboard = function () {
     pong.appRouter.navigate('', { trigger: true });
+    pong.collections.playerStandings.fetch();
+    pong.collections.recentMatches.fetch();
 };
--- spec/javascripts/reloadDashboardSpec.js
+++ spec/javascripts/reloadDashboardSpec.js
@@ -1,14 +1,30 @@
 describe('reloadDashboard', function () {
     beforeEach(function () {
-        pong.appRouter = new pong.AppRouter({
-            collections: []
-        });
+        pong.appRouter = new pong.AppRouter();
+        pong.collections = {};
+        pong.collections.playerStandings = new Backbone.Collection();
+        pong.collections.recentMatches = new Backbone.Collection();
+
+        spyOn(pong.collections.playerStandings, 'fetch');
+        spyOn(pong.collections.recentMatches, 'fetch');
     });
 
     it('routes to the root path', function () {
         spyOn(pong.appRouter, 'navigate');
-
         pong.reloadDashboard();
+
         expect(pong.appRouter.navigate).toHaveBeenCalledWith('', { trigger: true });
+    });
+
+    it('calls fetch on the playerStandings collection', function () {
+        pong.reloadDashboard();
+
+        expect(pong.collections.playerStandings.fetch).toHaveBeenCalled();
+    });
+
+    it('calls fetch on the recentMatches collection', function () {
+        pong.reloadDashboard();
+
+        expect(pong.collections.recentMatches.fetch).toHaveBeenCalled();
     });
 });
