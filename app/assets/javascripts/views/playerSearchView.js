fd6980d9c24f71ccb29010773291b5a8ec9c7a96
Ehren Murdick and John Ryan <pair+ehren+john@pivotallabs.com>
2014-04-16 16:35:04 -0700

    Update arrow navigation of player search list
    - use same highlight color as mouseover
    - only have one element highlighted when mousing over
    - what's a test?
    
    [#69208728]

--- app/assets/javascripts/views/playerSearchView.js
+++ app/assets/javascripts/views/playerSearchView.js
@@ -22,6 +22,7 @@ pong.PlayerSearchView = Backbone.View.extend({
     if (this.active)
       this.options().eq(this.selectedIndex).addClass('selected');
     this.$el.on('mousedown', _.bind(this.mousedownHandler, this));
+    this.$el.on('mouseover', _.bind(this.mouseOverHandler, this));
   },
 
   mousedownHandler: function (e) {
@@ -31,30 +32,54 @@ pong.PlayerSearchView = Backbone.View.extend({
     }
   },
 
-  selectNext: function() {
+  mouseOverHandler: function(e) {
+      if(e.target.localName === 'li') {
+          this.deactivateKeyboardNavigation();
+          this.deselect();
+          $(e.target).addClass('selected');
+      }
+  },
+
+  deselect: function() {
+      this.$el.find('.selected').removeClass('selected');
+  },
+
+  deactivateKeyboardNavigation: function() {
+      this.active = false;
+      this.selectedIndex = -1;
+      this.deselect();
+      this.$el.removeClass('active');
+  },
+
+  activateKeyboardNavigation: function() {
     this.active = true;
-    this.selectedIndex += 1;
-    this.constrainIndex();
+    this.$el.addClass('active');
+  },
+
+  selectNext: function() {
+      this.activateKeyboardNavigation();
+      this.selectedIndex += 1;
+      this.constrainIndex();
   },
 
   selectPrev: function() {
-    this.selectedIndex -= 1;
-    this.constrainIndex();
+      this.activateKeyboardNavigation();
+      this.selectedIndex -= 1;
+      this.constrainIndex();
   },
 
   enterSelected: function() {
-    var li = this.options().eq(this.selectedIndex);
-    this.onMousedownCallback(li.text());
-    this.collection.reset();
-    this.active = false;
-    this.selectedIndex = -1;
+      var li = this.options().eq(this.selectedIndex);
+      this.onMousedownCallback(li.text());
+      this.collection.reset();
+      this.deactivateKeyboardNavigation();
   },
 
   constrainIndex: function() {
-    this.selectedIndex = Math.max(0, Math.min(this.selectedIndex, this.options().length - 1));
+      this.selectedIndex = Math.max(0, Math.min(this.selectedIndex, this.options().length - 1));
   },
 
   options: function() {
-    return this.$el.find('li');
+      return this.$el.find('li');
   },
 });
--- spec/javascripts/playerSearchViewSpec.js
+++ spec/javascripts/playerSearchViewSpec.js
@@ -24,22 +24,47 @@ describe('Player Search View', function () {
         });
     });
 
-    describe('selectNext', function() {
+    describe('arrow navigation', function() {
         beforeEach(function() {
             this.view = new pong.PlayerSearchView({
                 onClickCallback: function() { return null; },
             });
             this.view.collection.reset([
-                new pong.Player({name: 'foobar'})
+                new pong.Player({name: 'foobar'}),
+                new pong.Player({name: 'foobaz'}),
             ]);
             expect(this.view.options().length).not.toEqual(0);
         });
 
-        it('highlights the next result', function() {
-            expect(this.view.options().filter('.selected').length).toEqual(0);
-            this.view.selectNext();
-            this.view.render();
-            expect(this.view.options().index('.selected')).not.toEqual(0);
+        describe('selectNext', function() {
+            it('highlights the next result', function() {
+                expect(this.view.options().filter('.selected').length).toEqual(0);
+
+                this.view.selectNext();
+                expect(this.view.selectedIndex).toEqual(0);
+
+                this.view.selectNext();
+                expect(this.view.selectedIndex).toEqual(1);
+
+                this.view.render();
+                expect(this.view.options().index('.selected')).not.toEqual(0);
+            });
+        });
+
+        describe('selectPrev', function() {
+            beforeEach(function() {
+                this.view.selectedIndex = 1;
+            });
+
+            it('highlights the previous result', function() {
+                expect(this.view.options().filter('.selected').length).toEqual(0);
+
+                this.view.selectPrev();
+                expect(this.view.selectedIndex).toEqual(0);
+
+                this.view.render();
+                expect(this.view.options().index('.selected')).not.toEqual(0);
+            });
         });
     });
 });
