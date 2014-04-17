56bd6b632650380492db3a729e68c2223b4179e1
John Ryan and Mariana Lenetis <pair+john+mariana@pivotallabs.com>
2014-04-17 16:03:47 -0700

    TournamentWinnerPickerSpec is more readable.

--- spec/tournament_actions/tournament_winner_picker_spec.rb
+++ spec/tournament_actions/tournament_winner_picker_spec.rb
@@ -4,36 +4,44 @@ describe TournamentWinnerPicker do
   subject(:picker) { TournamentWinnerPicker.new }
 
   describe '#pick_winners' do
-    context 'without tournaments' do
-      let(:old_match_date) { DateTime.new(2014, 1, 15) }
-      let(:new_match_date) { DateTime.new(2014, 2, 15) }
+    context 'there are no persisted tournaments' do
+      let(:during_tournament_1) { DateTime.new(2014, 1, 15) }
+      let(:during_tournament_2) { DateTime.new(2014, 2, 15) }
       let!(:old_match) { Match.create(
           winner_key: 'bob',
           loser_key: 'alice',
-          created_at: old_match_date) }
+          created_at: during_tournament_1) }
       let!(:new_match) { Match.create(
           winner_key: 'alice',
           loser_key: 'bob',
-          created_at: new_match_date) }
+          created_at: during_tournament_2) }
 
-      it 'creates tournaments with the correct winners' do
+      it 'creates the right number of tournaments' do
         expect {
           picker.pick_winners
         }.to change(Tournament, :count).by(2)
+      end
 
+      it 'makes each tournament one week long' do
+        tournament_1 = Tournament.first
+        expect(tournament_1.start_time).to eq(during_tournament_1.beginning_of_week)
+        expect(tournament_1.end_time).to eq(during_tournament_1.end_of_week)
+
+        tournament_2 = Tournament.last
+        expect(tournament_2.start_time).to eq(during_tournament_2.beginning_of_week)
+        expect(tournament_2.end_time).to eq(during_tournament_2.end_of_week)
+      end
+
+      it 'picks the correct winners' do
         tournament_1 = Tournament.first
         expect(tournament_1.winner_key).to eq('bob')
-        expect(tournament_1.start_time).to eq(old_match_date.beginning_of_week)
-        expect(tournament_1.end_time).to eq(old_match_date.end_of_week)
 
         tournament_2 = Tournament.last
         expect(tournament_2.winner_key).to eq('alice')
-        expect(tournament_2.start_time).to eq(new_match_date.beginning_of_week)
-        expect(tournament_2.end_time).to eq(new_match_date.end_of_week)
       end
     end
 
-    context 'with tournaments' do
+    context 'there are persisted tournaments' do
     end
   end
 
