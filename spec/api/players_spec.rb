4e4da9797710487ac9bda69b78046eb975252b68
Ehren Murdick and Mariana Lenetis <pair+emurdick+mariana@pivotallabs.com>
2014-04-23 15:28:29 -0700

    changed example api docs to have a uuid in the key field like the actual json response will

--- spec/api/players_spec.rb
+++ spec/api/players_spec.rb
@@ -2,14 +2,16 @@ require 'spec_helper'
 
 describe 'Players api' do
   describe 'GET /api/players.js', :show_in_doc do
+    let(:ehren_key) { SecureRandom.uuid }
+    let(:patrick_key) { SecureRandom.uuid }
     it 'renders players', :show_in_doc do
-      Player.create name: 'Ehren', mean: 1112, key: 'ehren'
-      Player.create name: 'Patrick', mean: 1200, key: 'patrick'
+      Player.create name: 'Ehren', mean: 1112, key: ehren_key
+      Player.create name: 'Patrick', mean: 1200, key: patrick_key
       get api_players_path(format: :json)
       expect(response).to be_success
       expect(response_json).to eq({players: [
-        {"name"=>"Ehren", "mean"=>1112, "key"=>"ehren"},
-        {"name"=>"Patrick", "mean"=>1200, "key"=>"patrick"}
+        {"name"=>"Ehren", "mean"=>1112, "key"=>ehren_key},
+        {"name"=>"Patrick", "mean"=>1200, "key"=>patrick_key}
       ]})
     end
   end
