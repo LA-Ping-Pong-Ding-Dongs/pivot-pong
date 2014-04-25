677c32393000210eee799d9195cf7c207594c2a2
Ehren Murdick <pair+emurdick@pivotallabs.com>
2014-04-25 10:15:30 -0700

    use respond_with instead of custom action handling stuff

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -9,49 +9,26 @@ class BaseController < ApplicationController
 
   def index
     self.collection = service.get_page(params[:page])
-    respond_to do |format|
-      format.json { render json: { results: collection.as_json } }
-      format.html { respond_with collection }
-    end
   end
 
   def show
     self.resource = finder
-    respond_to do |format|
-      format.json { render json: resource }
-      format.html { respond_with resource }
-    end
   end
 
   def edit
     self.resource = finder
-    respond_to do |format|
-      format.html { respond_with resource }
-    end
   end
 
   def create
     self.resource = service.new(safe_params)
-    respond_to do |format|
-      if resource.save
-        format.json { render json: resource, status: 201 }
-        format.html { redirect_to root_path }
-      else
-        format.json { render status: 400, json: resource.as_json.merge(resource.errors.as_json) }
-        format.html { redirect_to root_path, alert: resource.error_messages }
-      end
-    end
+    resource.save
+    respond_with(resource)
   end
 
   def update
     self.resource = finder
-    respond_to do |format|
-      if resource.update_attributes(safe_params)
-        format.html { redirect_to resource }
-      else
-        format.html { render action: :edit }
-      end
-    end
+    resource.update_attributes(safe_params)
+    respond_with(resource)
   end
 
   private
--- spec/controllers/base_controller_spec.rb
+++ spec/controllers/base_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe BaseController do
+  class FakeModel
+    def self.singular_route_key; 'base'; end
+    def self.model_name; self; end
+    def to_param; '1'; end
+  end
+
   before do
     # adding routes to spec and abstract class
     # http://pivotallabs.com/adding-routes-for-tests-specs-with-rails-3/
@@ -17,10 +23,10 @@ describe BaseController do
     Rails.application.reload_routes!
   end
 
-
   let :service do
     double(:service)
   end
+
 
   let(:params) do
     { some: 'params' }
@@ -32,21 +38,22 @@ describe BaseController do
   end
 
   describe '#update' do
-    let(:model) { double(:model) }
+    let(:model) { FakeModel.new }
     context 'js response' do
       before do
-        allow(controller).to receive(:url_for).and_return('/model_path')
         allow(service).to receive(:find).and_return(model)
       end
 
       it 'redirects to model show page on success' do
         allow(model).to receive(:update_attributes).and_return(true)
+        allow(model.class).to receive(:model_name).and_return(FakeModel)
         patch :update, id: 1, model: { name: 'Mallomar' }
-        expect(response).to redirect_to('http://test.host/model_path')
+        expect(response).to redirect_to('http://test.host/base/1')
       end
 
       it 'renders edit with errors if save fails' do
         allow(model).to receive(:update_attributes).and_return(false)
+        allow(model).to receive(:errors).and_return([1])
         patch :update, id: 1, model: { name: 'Templeton' }
         expect(response).to render_template('edit')
       end
@@ -55,30 +62,29 @@ describe BaseController do
 
 
   describe '#create' do
-    let(:factory) { double('factory', error_messages: 'didnt work yo', as_json: {fake: 'persistence object'}) }
+    let(:model) { FakeModel.new }
 
     before do
-      allow(service).to receive(:new).and_return(factory)
+      allow(service).to receive(:new).and_return(model)
     end
 
     context 'responds to html' do
       describe 'success' do
         it 'does the same thing' do
-          allow(factory).to receive(:save).and_return(true)
+          allow(model).to receive(:save).and_return(true)
           post :create, params
 
-          expect(response).to redirect_to(root_path)
+          expect(response).to redirect_to('/base/1')
           expect(flash[:alert]).to be_blank
         end
       end
 
       describe 'failure' do
         it 'adds an alert to the flash' do
-          allow(factory).to receive(:save).and_return(false)
+          allow(model).to receive(:save).and_return(false)
           post :create, params
 
           expect(response).to be_redirect
-          expect(flash[:alert]).to be_present
         end
       end
     end
@@ -86,7 +92,8 @@ describe BaseController do
     context 'responds to json' do
       describe 'success' do
         it 'works' do
-          allow(factory).to receive(:save).and_return(true)
+          allow(model).to receive(:save).and_return(true)
+          allow(model).to receive(:as_json).and_return(fake: 'persistence object')
           xhr :post, :create, params.merge(format: :json)
 
           expect(response).to be_success
@@ -96,12 +103,12 @@ describe BaseController do
 
       describe 'failure' do
         it 'includes an error key in the response' do
-          allow(factory).to receive(:save).and_return(false)
-          allow(factory).to receive(:errors).and_return(some: 'error')
+          allow(model).to receive(:save).and_return(false)
+          allow(model).to receive(:errors).and_return(some: 'error')
           xhr :post, :create, params.merge(format: :json)
 
-          expect(response.status).to be(400)
-          expect(response.body).to eq('{"fake":"persistence object","some":"error"}')
+          expect(response.status).to be(422)
+          expect(response.body).to eq('{"errors":{"some":"error"}}')
         end
       end
     end
