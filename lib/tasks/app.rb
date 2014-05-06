ea6fe3d5316c9e9b7545192620ec5433bdc57bbd
John Ryan and Mariana Lenetis <pair+john+mariana@pivotallabs.com>
2014-05-06 11:27:36 -0700

    Extracted task to a gem

--- lib/tasks/app.rb
+++
@@ -1,8 +0,0 @@
-class App
-  attr_reader :name, :state
-
-  def initialize(name: , state: 'stopped')
-    @name = name
-    @state = state
-  end
-end
--- lib/tasks/route.rb
+++
@@ -1,8 +0,0 @@
-class Route
-  attr_reader :host, :domain, :app
-  def initialize(host, domain, app)
-    @host = host
-    @domain = domain
-    @app = app == '' ? nil : app
-  end
-end
